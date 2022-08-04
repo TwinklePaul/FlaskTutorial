2e317ad0b23b1de43eb847b958d41316362afc40
Twinkle <twinkle@MacBook-Pro.local>
2022-08-04 18:34:39 +0530

    Modified to reflect env var.

--- app.py
+++ app.py
@@ -3,7 +3,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, template_folder='templates')
 
-color = "tomato"
+# In terminal use the following command to set env variable
+# export APP_COLOR=powderblue
+color = os.environ.get("APP_COLOR")
 
 
 @app.route('/')
